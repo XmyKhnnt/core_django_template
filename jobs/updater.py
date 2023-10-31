7c0d0bf314dceb98646d09b4dbd2a79d4c517bbc
Khent Mark Dahay <km.dahay@rentermade.com>
2023-11-01 01:36:32 +0800

    change interval

--- jobs/updater.py
+++ jobs/updater.py
@@ -5,5 +5,5 @@ from .jobs import schedule_api
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(schedule_api, 'interval', seconds=1440)
+    scheduler.add_job(schedule_api, 'interval', seconds=10)
     scheduler.start()
